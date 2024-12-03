3118e79c5c6a6eddd5134d8f2322113fb1e0d37d
Adrian <axtrinee@gmail.com>
2024-12-03 08:24:17 -0800

    Update refrence.js

--- refrence.js
+++ refrence.js
@@ -1,1 +1,14 @@
-
+ function startTime() {
+          const today = new Date();
+          let h = today.getHours();
+          let m = today.getMinutes();
+          let s = today.getSeconds();
+          m = checkTime(m);
+          s = checkTime(s);
+          document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+          setTimeout(startTime, 1000);
+        }
+        
+        function checkTime(j) {
+          if (j < 10)  {j = "0" + j};  // add zero in front of numbers < 10
+          return j;
