1b2ba296f3be4f385ca93a8bfc985053d20e43f8
Adrian <axtrinee@gmail.com>
2025-02-23 20:53:45 -0600

    please work
    :sob:

---
+++ dyn.js
@@ -0,0 +1,290 @@
+ // i think this works?
+ const textOptions = [
+    "No more splarg :(",
+    "John Cena",
+    "Did you know there's no limit to the length of this?",
+    "Do you know who else-",
+    "Steam friend code: 1264049544",
+    "Thy stol my fucking   ky",
+    "Eight equal sign equal sign capital d",
+    "MEDIC!",
+    "Oh boy I sure I hope I don't have any DCMA emails…",
+    "I know where you live~",
+    "I know what you are~",
+    "I've come to make an announcement, shadow the hedgehog is a bitchass motherfucker. He pissed on my fucking wife, that's right he took his hedgehog quilly dick out and he pissed on my fucking wife and he said his dick was “this big” and I said that's disgusting, so I'm making a callout post on my twitter dot com. Shadow the hedgehog you got a small dick, it's the size of this wallnut except way smaller, and guess what? Here's what my dong looks like. That's right baby tall point, no quills and no pillows. Look at that it looks like two balls and a dong. He clubbed my wife so guess what? I'm gonna fuck the earth. That's right that's what you get my super laser piss. Expect I'm not gonna piss on the earth I'm gonna go higher IM PISSING ON THE MOON!! HOW DO YOU LIKE THAT OBAMA? I PISSED ON THE MOON YOU IDIOT! You have 23 hours before the piss droplets hit the fucking earth. Now get out of my fucking sight before I piss on you too.",
+    "Click for nothing",
+    "staff-furry-rp is NOT real",
+    "Five. Hundred. Cigarettes.",
+    "Make a news ticker? I don't have that many unique ideas.",
+    "This broke and didn't work 83 times and counting.",
+    "Isn't this just worse ripped.guide? Well yes, but it's updated and neater in my opinion.",
+    "C:/Windows/Users/Adrian/Documents/nothing_to_see_here.txt",
+    "C:/Windows/Users/Adrian/Desktop/guh/flargapiracysite.html",
+    "A shrimp (pl.: shrimp (US) or shrimps (UK)) is a crustacean (a form of shellfish) with an elongated body and a primarily swimming mode of locomotion  typically belonging to the Caridea or Dendrobranchiata of the order Decapoda, although some crustaceans outside of this order are also referred to as shrimp. The shrimp Palaemon serratus of the infraorder Caridea. More narrow definitions may be restricted to Caridea, to smaller species of either group or to only the marine species. Under a broader definition, shrimp may be synonymous with prawn, covering stalk-eyed swimming crustaceans with long, narrow muscular tails (abdomens), long whiskers (antennae), and slender legs.[1] Any small crustacean which resembles a shrimp tends to be called one.[2] They swim forward by paddling with swimmerets on the underside of their abdomens, although their escape response is typically repeated flicks with the tail driving them backwards very quickly. Crabs and lobsters have strong walking legs, whereas shrimp have thin, fragile legs which they use primarily for perching.[3]. Shrimp are widespread and abundant. There are thousands of species adapted to a wide range of habitats. They can be found feeding near the seafloor on most coasts and estuaries, as well as in rivers and lakes. To escape predators, some species flip off the seafloor and dive into the sediment.[3] They usually live from one to seven years.[4] Shrimp are often solitary, though they can form large schools during the spawning season.[3][5] They play important roles in the food chain and are an important food source for larger animals ranging from fish to whales. The muscular tails of many shrimp are edible to humans, and they are widely caught and farmed for human consumption. Commercial shrimp species support an industry worth 50 billion dollars a year,[3] and in 2010 the total commercial production of shrimp was nearly 7 million tonnes. Shrimp farming became more prevalent during the 1980s, particularly in China, and by 2007 the harvest from shrimp farms exceeded the capture of wild shrimp. There are significant issues with excessive bycatch when shrimp are captured in the wild, and with pollution damage done to estuaries when they are used to support shrimp farming. Many shrimp species are small as the term shrimp suggests, about 2 cm (0.79 in) long, but some shrimp exceed 25 cm (9.8 in). Larger shrimp are more likely to be targeted commercially and are often referred to as prawns, particularly in the Commonwealth of Nations and former British colonies.",
+    "I was nowhere during September 11th 2001",
+    "i need ideas :<",
+    "https://discord.gg/3zKJBDfDsY",
+    "this is NOT steam happy",
+    "France isn't real",
+    "https://youtu.be/JE37e1eK2mY?si=-KFKgdTFFoPvO1cG",
+    "Jerry's bait shop, you'd know the place.",
+    "yeag",
+    "I need an adult.",
+    "I am an adult.",
+    "I need an adultier adult",
+    "              ",
+    "I hate this.",
+    "Axtrine is NOT a femboy.",
+    "Update coming: never",
+    "No.",
+    "https://discord.gg/gzTEU3pAE7  <--- join",
+    "Frieza as head of foreign relations.",
+    "You thought there was a cycle here but no it was I DIO!",
+    "ara~ ara~",
+    "smoke_alarm_sound.wav",
+    "I have a shrimp fry!",
+    "const randomIndex = Math.floor(Math.random() * textOptions.length);console.log(randomIndex, textOptions[randomIndex]);  // debugging :>document.getElementById('dynamicText').innerText = textOptions[randomIndex];",
+    "java is very fun!",
+    "Line 79 is a lie.",
+    "Flarg eh?",
+    "thedoctor6926 is a femboy.",
+    "guh",
+    "heyyy, the top text works ^^^^^ yippee",
+    "'Is that the Albuquerque backround?!' Yes, yes it is.",
+    "Adobe flash o7",
+    "freaky",
+    "Math floor.",
+    "GREGORY OPEN THE DOOR, MY FAZBALLS ARE STUCK.",
+    "Remember when <h1 id='headertext'>said oh it seems were this far?</h1> as filler text?",
+    "well golly.",
+    "me when i lie",
+    "'Your either perfect or your not me' - Mr. Perfect Cell",
+    ".",
+    "penis",
+    "I got a bucket of chicken!",
+    "Your mother.",
+    "SUDUCE ME",
+    "How do I get my husband to stop going 'Goblin Mode' during sex? TLDR; My husband says 'Goblin Mode activated' when we start to have sex, growls and acts like a caveman, and then says 'Goblin Mode off'' when we stop, and then pretends not to remember afterward.I really love my husband and he's always been great in bed. But recently he's been acting really weird. So, a couple of days ago, my son went on a rampage through our house and said he was in 'Goblin Mode'. We didn't really know what to do with him, so we sent him to live with my parents so he can go to a special needs school. My husband a really great relationship with our son and loved him more than anything. Naturally, he was upset when he had to leave. He's an incredibly tough man, but this was the first time I've ever seen him cry. I think since then, he's been a little emotionally unwell. I've heard him muttering, 'Goblin' repeatedly when he didn't notice me, staring blankly into his food, and just going alone by himself to do who knows what. I feel awful for him, but we both agreed that this was for the best. Last night, the day after our son went away, we decided to have sex to relieve our stress. However, my husband said 'Goblin Mode activated', starting growling, and went wild having sex with me. Admittedly, it was some of the best and most experimental sex I've ever had, but I'm worried that something might be going on with my husband. Any advice? Edit: The problem isn't the 'Goblin Mode', it's that he could be ill",
+    "how do i kill someone?",
+    "You can't match my freak.",
+    "I cast: TUNGSTEN BALLSACK!",
+    "sjisfdof;/sd;'pjoieadioeasdiko'9psad'WOp'dSWDmJWOPDJWIDPOK <----- sorz cat stepped on keyboard",
+    "𓆏",
+    "The voices keep talking.",
+    "Hello, I have reached out to you today to remind you about your cars extended waranty.",
+    "STOP POSTING ABOUT 𓆏 I'M TIRED OF SEEING IT! My friends on 𓆏 send me memes, on 𓆏 it's fucking 𓆏, i was in a server, right? and ALL of the channels are just 𓆏. I-I showed my black hole to my 𓆏, and the 𓆏 i flipped it and i said 'Hey 𓆏, when the 𓆏 is 𓆏 HAHA 𓆏 𓆏 𓆏 𓆏 𓆏 𓆏 𓆏 *takes breath* 𓆏 𓆏 𓆏' I FUCKING LOOKED AT A TRASH CAN, I SAID 'THAT'S A BIT 𓆏', I LOOKED AT MY 𓆏, I THINK OF THE 𓆏'S 𓆏, AND I GO '𓆏, more like 𓆏' *takes breath* 𓆏𓆏𓆏𓆏𓆏𓆏𓆏𓆏𓆏𓆏𓆏",
+    "4 months until 2013",
+    "i cant href :(",
+    "link(bside.html)",
+    "𓆏²",
+    "Help, Axtrine has trapped me in this website, I don't know how long I've been in here but you need to help me! I have a wife and kids! ALL I SAID WAS HIS SHOES LOOKED FUNNY!",
+    "Hi Squidward.",
+    "All religious texts are false because they don't mention Jägermeister.",
+    "you see that little /index.html? change that to /bside.html",
+    "*",
+    "I'm farted.",
+    "your    fat.",
+    "Check your floorboards",
+    "there is an armed shotgun trap at your front door",
+    "172.21.156.22",
+    "there are anti-personel landmines on your lawn",
+    "i have an artillery cannon aimed at your doorbell",
+    "you will die in 6 days",
+    "<a href='door.html' target='_blank'>The door.</a>",
+    "i CAN href :)",
+    "no one is coming to save you.",
+    "fun fact: 19 furries ejaculated on a pizza at a furry convention and left it out in the lobby in 2021",
+    "fun fact: there are catacombs under every government building in the us",
+    "fun fact: there is no one behind you.",
+    "watch yo tone mf",
+    "i will cut a hole in your chest, puncture your skull with hooks, and pull your head out through the hole",
+    "died of ball fart",
+    "fun fact: the man in the suit is not real.",
+    "i love playing in mud and eating sand",
+    "yeah kill this guy with hammers.... right here....",
+    "LOCK TF IN",
+    "you have $145,850 in unpaid taxes and the irs is coming for you",
+    "_i__er",
+    "i have chronic diarrhea",
+    "conductor we have a problem",
+    "please officer i cant breathe",
+    "i swear im not drunk, occifer",
+    "fun fact: the pills are not doing what you think they are.",
+    "fun fact: loop loop loop loop loop loop loop",
+    "fun fact: no no no no no no no",
+    "me explaining when the are and you when the no and you and the are and the is are when the the the wehn the are is i i i and the am is the y and you arewhen the are and you when the no and you and the are and the is are when the no and you and the are and the ius are when the the the when the are is i i i i i and the am is the y and you are",
+    "nei man?? what yu say men? what you say me? FAKC YOU MAN. FACK YOU.",
+    "beta is so silly",
+    "thanks beta for the deranged texts",
+    "WHY DO THEY CALL IT A RESTROOM IM FIGHTING FOR MY LIFE IN HERE",
+    "fun fact: the dog is outside your house.",
+    "fun fact: no one is watching you.",
+    "fun fact: the deer.",
+    "fun fact: eyes",
+    "39 buried, 0 found",
+    "fun fact: no one will ever know where you went",
+    "fun fact: every memory you have ever had is fake",
+    "fun fact: i love you.",
+    "kys (keep yourself safe)",
+    "gibby goober flibby womp",
+    "schmeckle tickle zippy zap",
+    "gowunga bowunga vuwigga womp",
+    "the fitness gram pacer test is a multi-stage aerobic capacity test",
+    "fun fact: BREAK THE CYCLE",
+    "fun fact: human flesh takes around 10 days to rot",
+    "fun fact: there are bugs under your skin. cut your skin open.  D I G  T H E M  O U T .",
+    "fun fact: stop breathing.",
+    "pthnpul wbaapun aopz aoyvbno h kljvkly qbza av ayhuzshal pa ohoh fvb kbtihzz kbti pkpva zabwpk",
+    "fun fact: breaking your bones would be very fun",
+    "fun fact: there is no one hiding around the corner",
+    "fun fact: you are all alone. exposed.  V U L N E R A B L E .",
+    "HELL IS FULL. BLOOD IS FUEL.",
+    "my balls itch",
+    "fun fact: you are not safe.",
+    ":p",
+    ";p",
+    "<img src='assets/Steamhappy.png'style='width:32px;height:32px;'>",
+    "<audio controls loop autoplay src='melon.mp3'></audio>",
+    "video? no.",
+   "<audio loop autoplay src='weird.mp3'>'style='width:1px;height:1px;'</audio>",
+   "gibby goober flibby womp schmeckle tickle zippy zap gowunga bowunga vuwigga womp",
+   "TACO BELLLLLLLL",
+   "What was this website for again? I forgot.",
+   "Sir this is a Wendy's",
+   "Mods, log his IP",
+   "eat an anvil"
+];
+// Keybinds
+document.addEventListener('keydown', (event) => {
+if (event.key === 'R' || event.key === 'r') {
+startAutoRotate(); // Start auto-rotate
+} else if (event.key === 'P' || event.key === 'p') {
+pauseAutoRotate(); // Pause auto-rotate
+} else if (event.key === 'D' || event.key === 'd') {
+loadText(90); // Cheating are we? 
+}  else if (event.key === ' ') { 
+loadRandomText(); 
+}  else if (event.key === 'M' || event.key === 'm') { 
+loadText(144); // wrong
+}  else if (event.key === 'S' || event.key === 's') { 
+loadText(143); // it was all a lie  
+}  else if (event.key === 'W' || event.key === 'w') { 
+loadText(146); // dun dun dunnnnnnnnnn  
+// i hate myself, it's evident
+
+
+} 
+});
+
+let autoRotateInterval = null;
+let currentIndex = 0; 
+let isAutoRotateActive = false; 
+
+function startAutoRotate() {
+if (isAutoRotateActive) {
+console.log("Auto-rotate is already active.");
+return;
+}
+
+isAutoRotateActive = true;
+autoRotateInterval = setInterval(() => {
+loadText(currentIndex);
+currentIndex = (currentIndex + 1) % textOptions.length; // Loop back to start
+console.log(`Rotating to index: ${currentIndex}`);
+}, 2000);
+
+console.log("Auto-rotate started.");
+updateRotationStatus(true);
+}
+
+
+function pauseAutoRotate() {
+if (autoRotateInterval) {
+clearInterval(autoRotateInterval);
+autoRotateInterval = null;
+isAutoRotateActive = false;
+console.log("Auto-rotate paused.");
+updateRotationStatus(false);
+}
+}
+function loadText(index) {
+if (index < 0 || index >= textOptions.length) {
+console.error(`Invalid index: ${index}. Must be between 0 and ${textOptions.length - 1}.`);
+return;
+}
+document.getElementById('dynamicText').innerText = textOptions[index];
+}
+
+function updateRotationStatus(isActive) {
+const statusElement = document.getElementById('rotationStatus');
+if (statusElement) {
+statusElement.innerText = isActive ? "Auto-rotate: ON" : "Auto-rotate: OFF";
+}
+}
+
+// Text-to-speech
+const textCounters = new Array(textOptions.length).fill(0);
+function speakText(index) {
+const utterance = new SpeechSynthesisUtterance(textOptions[index]);
+speechSynthesis.speak(utterance);
+}
+// Text loading
+function loadRandomText() {
+const randomIndex = Math.floor(Math.random() * textOptions.length);
+loadText(randomIndex);
+
+}
+console.log("uhhhh, hi?")
+
+function loadText(index) {
+const dynamicText = document.getElementById('dynamicText');
+if (index >= 0 && index < textOptions.length) {
+dynamicText.classList.add('hidden'); 
+
+setTimeout(() => {
+dynamicText.innerHTML = textOptions[index];
+dynamicText.classList.remove('hidden'); 
+}, 300); 
+
+dynamicText.innerHTML = escapeHTML(textOptions[index]);
+} else {
+// speakText(index);
+console.error(`Index out of bounds. Please provide a number between 0 and ${textOptions.length - 1}.`);
+}
+// Cycle on keypress
+}
+document.addEventListener('keydown', (event) => {
+
+
+}
+
+);
+// i done forgor
+function escapeHTML(html) {
+const div = document.createElement('div');
+div.textContent = html;
+return div.innerHTML;
+}
+loadRandomText();
+
+window.loadText = loadText;
+window.loadRandomText = loadRandomText;
+
+// let autoRotateInterval = null;
+
+// function startAutoRotate(interval = 5000) {
+//  if (autoRotateInterval) clearInterval(autoRotateInterval);
+//  autoRotateInterval = setInterval(() => {
+//    const randomIndex = Math.floor(Math.random() * textOptions.length);
+//    loadText(randomIndex);
+//  }, interval);
+// }
+// textCounters[index]++;
+// console.log(`"${textOptions[index]}" has been displayed ${textCounters[index]} times.`);
+
+function stopAutoRotate() {
+clearInterval(autoRotateInterval);
+}
+
+// Start auto-rotation by default 
+// startAutoRotate();
