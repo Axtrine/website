71836277a47be82ccb5b36943bf297e6e2e87837
Adrian <axtrinee@gmail.com>
2025-08-19 12:55:42 -0500

    fuckass button broke, i'm crying myself to sleep

--- button.js
+++ button.js
@@ -2,8 +2,6 @@ let clickCount = 0; // more math <-- it wasnt bad :D
 function incrementCounter() {
     clickCount++; 
     document.getElementById("counterDisplay").innerText = `Button for clicking clicked ${clickCount} times.`; // Display the count
-    if (clickCount === 10) {
-        console.log("counter is strictly equal to 10");
 }
 
 function closePage() {
